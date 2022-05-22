b9ac17eb3515eb91ce75142fa8ec3a05e75a0940
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-05-22 16:52:43 +0100

    test file paths

--- pages/api/countries.js
+++ pages/api/countries.js
@@ -6,6 +6,11 @@ export default async function handler(req, res) {
   const basePath = path.join(__dirname, `../../../../data`)
   const allDetailedData = await loadCsv(`${basePath}/NFA_2018_Detailed.csv`)
 
+  console.log(__dirname)
+  console.log(basePath)
+  console.log(path.join(__dirname, `../../../data`))
+  console.log(path.join(__dirname, `../../data`))
+
   const countries = allDetailedData.map((entry) => ({
     name: entry.Country,
     region: entry.Region,
