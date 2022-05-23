43a6edf2e0b9c6eb41d1eff1989ac040d0b31e69
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-05-23 21:56:35 +0100

    add loading state to Select

--- components/Select.js
+++ components/Select.js
@@ -3,6 +3,7 @@ import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 
+import { Ring } from '@uiball/loaders'
 import clsx from 'clsx'
 
 export default function Select({ selected, onChange, placeholder, options }) {
@@ -31,44 +32,53 @@ export default function Select({ selected, onChange, placeholder, options }) {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {options.map((option) => (
-                  <Listbox.Option
-                    key={option.id}
-                    className={({ active }) =>
-                      clsx(
-                        active ? 'text-white bg-green-700' : 'text-gray-900',
-                        'cursor-default select-none relative py-2 pl-3 pr-9',
-                      )
-                    }
-                    value={option}
-                  >
-                    {({ selected, active }) => (
-                      <>
-                        <div className="flex items-center">
-                          <span
-                            className={clsx(
-                              selected ? 'font-semibold' : 'font-normal',
-                              'block truncate',
-                            )}
-                          >
-                            {option.name}
-                          </span>
-                        </div>
+                {options.length > 0 ? (
+                  options.map((option) => (
+                    <Listbox.Option
+                      key={option.id}
+                      className={({ active }) =>
+                        clsx(
+                          active ? 'text-white bg-green-700' : 'text-gray-900',
+                          'cursor-default select-none relative py-2 pl-3 pr-9',
+                        )
+                      }
+                      value={option}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <div className="flex items-center">
+                            <span
+                              className={clsx(
+                                selected ? 'font-semibold' : 'font-normal',
+                                'block truncate',
+                              )}
+                            >
+                              {option.name}
+                            </span>
+                          </div>
 
-                        {selected ? (
-                          <span
-                            className={clsx(
-                              active ? 'text-white' : 'text-primary-dark',
-                              'absolute inset-y-0 right-0 flex items-center pr-4',
-                            )}
-                          >
-                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
+                          {selected ? (
+                            <span
+                              className={clsx(
+                                active ? 'text-white' : 'text-primary-dark',
+                                'absolute inset-y-0 right-0 flex items-center pr-4',
+                              )}
+                            >
+                              <CheckIcon
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                              />
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Listbox.Option>
+                  ))
+                ) : (
+                  <div className="flex justify-center py-2">
+                    <Ring size={32} lineWeight={5} speed={2} color="#00092C" />
+                  </div>
+                )}
               </Listbox.Options>
             </Transition>
           </div>
