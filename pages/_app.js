7f43d9a5bf4c6c27085d7d84412d859b4d9ab4b1
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-06-23 01:13:35 +0100

    use title case

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Our planet wrapped</title>
+        <title>Our Planet Wrapped</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
