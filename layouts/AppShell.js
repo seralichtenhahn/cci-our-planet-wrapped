82c16d7521b22137ec2aeb0b549662e3f559bbc1
Serafin Lichtenhahn <s.lichtenhahn0420201@arts.ac.uk>
2022-06-27 20:48:19 +0200

    safari fixes

--- layouts/AppShell.js
+++ layouts/AppShell.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
   return (
     <div className="w-screen h-screen font-sans md:space-x-4 bg-gradient-to-r from-gray-700 via-gray-900 to-black md:flex md:items-center md:justify-center md:p-8">
       {isSlideShow && <NextPrevLink variant="prev" />}
-      <main className="h-full relative w-full bg-primary-dark overflow-y-auto overflow-x-hidden md:max-w-max md:shadow-md md:shadow-primary-dark/50 md:aspect-[9/16] md:rounded-2xl">
+      <main className="h-full relative w-full md:w-auto bg-primary-dark overflow-y-auto overflow-x-hidden md:max-w-max md:shadow-md md:shadow-primary-dark/50 md:aspect-[9/16] md:rounded-2xl">
         {children}
       </main>
       {isSlideShow && <NextPrevLink variant="next" />}
